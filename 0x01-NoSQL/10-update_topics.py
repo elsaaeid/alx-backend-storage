d02ec5d03017834fd8f9fcc1262a7e58bdfe44b7
elsaaeid <saidsadaoy@gmail.com>
2024-07-16 13:47:32 +0000

    deploy changes

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module that change school topics.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Function that changes all topics of a collection's document based on
+        the name.
+    """
+    mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
+    )
---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module that returns the list of school having a specific topic.
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Function that returns the list of school having a specific topic.
+    """
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
