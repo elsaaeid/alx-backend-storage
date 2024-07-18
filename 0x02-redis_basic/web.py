e6184535bd72d78af0f33471731da4acdee59d87
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-07-18 03:16:06 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,26 +6,27 @@ Module for Implementing an expiring web cache and tracker
 from functools import wraps
 import redis
 import requests
-from typing import Callable
 
 redis_ = redis.Redis()
 
 
-def count_requests(method: Callable) -> Callable:
+def count_requests(method):
     """Function for counting requests"""
     @wraps(method)
-    def wrapper(url):  # sourcery skip: use-named-expression
-        """Wrapper for decorator"""
-        redis_.incr(f"count:{url}")
-        cached_html = redis_.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
         html = method(url)
-        redis_.setex(f"cached:{url}", 10, html)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
         return html
-
     return wrapper
-
 
 @count_requests
 def get_page(url: str) -> str:
